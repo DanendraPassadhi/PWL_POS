9d230b5331b4e3c4a2751056f9bd0a88143962cd
Danendra Nayaka Passadhi <danendra.passadhi71@gmail.com>
2025-03-11 05:09:59 +0700

    feat : Jobsheet 5 (jQuery Datatable Implementation in AdminLTE)

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SalesController;
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,36 +19,15 @@ use App\Http\Controllers\WelcomeController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/', [HomeController::class, 'index'])->name('home');
-
-Route::prefix('category')->group(function () {
-    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
-    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
-    Route::get('/home-care', [ProductController::class, 'homeCare']);
-    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
-});
-
-Route::get('/user/{id}/name/{name}', [UserController::class, 'show']);
-
-Route::get('/sales', [SalesController::class, 'index'])->name('transaction');
-
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
-
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
-
 Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']);              // Menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']);          // Menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [UserController::class, 'create']);       // Menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']);             // Menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']);           // Menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);      // Menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']);         // Menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']);     // Menghapus data user
+});
